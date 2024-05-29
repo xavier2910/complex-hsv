3e8f947775d215dcf4294308bdeb8f325eb2c13c
xavier2910 <x.distelzweig@gmail.com>
2024-05-28 21:14:28 -0500

    updated help text

--- app/Main.hs
+++ app/Main.hs
@@ -1,32 +1,36 @@
-{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module Main (main) where
 
 import Import
-import Run
-import RIO.Process
 import Options.Applicative.Simple
 import qualified Paths_complex_hsv
+import RIO.Process
+import Run
 
 main :: IO ()
 main = do
-  (options, ()) <- simpleOptions
-    $(simpleVersion Paths_complex_hsv.version)
-    "Header for command line arguments"
-    "Program description, also for command line arguments"
-    (Options
-       <$> switch ( long "verbose"
-                 <> short 'v'
-                 <> help "Verbose output?"
-                  )
-    )
-    empty
+  (options, ()) <-
+    simpleOptions
+      $(simpleVersion Paths_complex_hsv.version)
+      "I really don't know what belongs here."
+      "Graphs a complex function: inputs are 1/s * e^(h*i), outputs are x + y*i, ie, color indicates where a point came from, its position, where it goes."
+      ( Options
+          <$> switch
+            ( long "verbose"
+                <> short 'v'
+                <> help "Verbose output?"
+            )
+      )
+      empty
   lo <- logOptionsHandle stderr (optionsVerbose options)
   pc <- mkDefaultProcessContext
   withLogFunc lo $ \lf ->
-    let app = App
-          { appLogFunc = lf
-          , appProcessContext = pc
-          , appOptions = options
-          }
+    let app =
+          App
+            { appLogFunc = lf
+            , appProcessContext = pc
+            , appOptions = options
+            }
      in runRIO app run
